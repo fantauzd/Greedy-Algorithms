5985ebea9fa5ebbae78455a8825a6508f51c8901
fantauzd <fantauzd@oregonstate.edu>
2024-05-05 18:36:58 -0400

    added loop to look at each activity

--- Activity.py
+++ Activity.py
@@ -12,3 +12,15 @@
 # for example, 'cook' and 'paint'. You can choose activities that immediately start
 # after the previous one ends, for example, 'Play Golf' and 'Paint'.
 
+def activity_selection(activities, starts, ends):
+    """
+    Solves activity selection problem where end times are in sorted order (ASC)
+    and activities and times correspond by index.
+    :param activities: a list of activities
+    :param starts: a list of start times
+    :param ends: a list of end times
+    :return: a list with the maximum salection of activites
+    """
+    result = []
+    blocked_time = 0
+    for i in range(len(activities)):
