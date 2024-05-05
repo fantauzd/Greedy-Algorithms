e9186b6240efe255aef223fb65a892da39391ec4
fantauzd <fantauzd@oregonstate.edu>
2024-05-05 18:32:19 -0400

    added activity problem

---
+++ Activity.py
@@ -0,0 +1,14 @@
+# You are given a list of activities {a1, a2,...an} with their start times [s1, s2,...sn]
+# and end times [e1,e2,....en].
+#
+# Example: activities: {Play Golf, Paint, Cook, Sleep, Jog, Write Code, Eat}
+#
+# start times: [1, 3, 1, 3, 4, 6, 8]
+#
+# end times: [3, 4, 4, 6, 6, 9, 9]
+#
+# You are equally interested in all of them. Your goal is to maximize
+# the number of activities that you can perform. You cannot choose overlapping activities,
+# for example, 'cook' and 'paint'. You can choose activities that immediately start
+# after the previous one ends, for example, 'Play Golf' and 'Paint'.
+
