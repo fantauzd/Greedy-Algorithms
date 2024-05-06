e27a442e0e632c27d2e6c1789ce4a77cb23f9975
fantauzd <fantauzd@oregonstate.edu>
2024-05-06 15:38:37 -0400

    solved dog feed problem

--- FeedDog.py
+++ FeedDog.py
@@ -10,3 +10,31 @@
 # You cannot give same biscuit to two dogs.
 # Each dog can get only one biscuit.
 
+def feedDog(hunger_level, biscuit_size):
+    """
+    Solves the feed dog problem and returns the number of dogs that are satisified.
+    :param hunger_level: an array where each index represents the hunger level of a dog
+    :param biscuit_size: an array where each index represents the size of a biscuit
+    :return: int, number of dogs that are satisfied
+    """
+    # sort the input arrays in ascending order
+    hunger_level.sort()        # timsort in O(nlogn)
+    biscuit_size.sort()
+    # initialize variables to track fed dogs and current dog
+    result = 0
+    dog = 0
+    # for each biscuit we have
+    for biscuit in range(len(biscuit_size)):
+        # start with the largest biscuit left
+        j = len(biscuit_size) - 1
+        # find the smallest biscuit that will satisfy the dog or use the smallest biscuit
+        while j > 0 and hunger_level[dog] != biscuit_size[j]:
+            j -= 1
+        # feed the biscuit to the dog
+        hunger_level[dog] -= biscuit_size[j]
+        biscuit_size.remove(biscuit_size[j])
+        # if the dog was satisfied move to next dog
+        if hunger_level[dog] <= 0:
+            dog += 1
+            result += 1
+
