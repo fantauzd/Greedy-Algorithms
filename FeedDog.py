4a6fc78e92c1ef279a0768d3bdbb803f9963eb62
fantauzd <fantauzd@oregonstate.edu>
2024-05-06 15:59:58 -0400

    fixed feed dog problem to work when each dog can only have 1 biscuit

--- FeedDog.py
+++ FeedDog.py
@@ -1,3 +1,5 @@
+# Dominic Fantauzzo
+
 # You are a pet store owner and you own few dogs. Each dog has a specific hunger level given
 # by array hunger_level [1..n] (ith dog has hunger level of hunger_level [i]). You have couple of
 # dog biscuits of size given by biscuit_size [1…m]. Your goal to satisfy maximum number of
@@ -23,22 +25,23 @@ def feedDog(hunger_level, biscuit_size):
     # initialize variables to track fed dogs and current dog
     result = 0
     dog = 0
-    # for each biscuit we have
-    for biscuit in range(len(biscuit_size)):
-        # start with the largest biscuit left
-        j = len(biscuit_size) - 1
-        # find the smallest biscuit that will satisfy the dog or use the smallest biscuit
-        while j > 0 and hunger_level[dog] != biscuit_size[j]:
-            j -= 1
+    # we repeat until we run our of biscuits or dogs
+    for exchange in range(min(len(biscuit_size), len(hunger_level))):
+        # start with the smallest biscuit left
+        j = 0
+        # find the smallest biscuit that will satisfy the dog or use the largest biscuit
+        while j < (len(biscuit_size)-1) and hunger_level[dog] > biscuit_size[j]:
+            j += 1
         # feed the biscuit to the dog
         hunger_level[dog] -= biscuit_size[j]
         biscuit_size.remove(biscuit_size[j])    #O(n) time complexity
-        # if the dog was satisfied move to next dog
+        # if the dog was satisfied then count it
         if hunger_level[dog] <= 0:
-            dog += 1
             result += 1
+        # we can only give the dog one biscuit, so move to next dog
+        dog += 1
 
     return result
 
 if __name__ == "__main__":
-    print(feedDog([4,3, 6], [4,4, 1, 2, 1, 1]))
+    print(feedDog([1, 2, 3, 4, 5], [1,2, 4, 3, 1, 4]))
