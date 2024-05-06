6954d64b8895dca621a0557f8f0a5dd61ce12758
fantauzd <fantauzd@oregonstate.edu>
2024-05-05 23:15:54 -0400

    added FeedDog problem description

---
+++ FeedDog.py
@@ -0,0 +1,12 @@
+# You are a pet store owner and you own few dogs. Each dog has a specific hunger level given
+# by array hunger_level [1..n] (ith dog has hunger level of hunger_level [i]). You have couple of
+# dog biscuits of size given by biscuit_size [1…m]. Your goal to satisfy maximum number of
+# hungry dogs. You need to find the number of dogs we can satisfy.
+# If a dog has hunger hunger_level[i], it can be satisfied only by taking a biscuit of size
+# biscuit_size [j] >= hunger_level [i] (i.e biscuit size should be greater than or equal to hunger
+# level to satisfy a dog.)
+# If no dog can be satisfied return 0.
+# Conditions:
+# You cannot give same biscuit to two dogs.
+# Each dog can get only one biscuit.
+
