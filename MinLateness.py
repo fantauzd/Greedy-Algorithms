4ab714c71f59ddf3f4f17a4583e58ba60c7e39b9
fantauzd <fantauzd@oregonstate.edu>
2024-05-09 17:35:44 -0400

    added min lateness problem

---
+++ MinLateness.py
@@ -0,0 +1,12 @@
+# Problem: You have a list of assignments [A, B, C, D]. You are provided with the time that it takes to solve these assignments, timetaken[t1, t2, t3, t4] and the deadline of each of the assignment, deadline[d1, d2, d3, d4].
+#
+# Where t1 corresponds to the time taken to complete Assignment A, which is due on d1.
+#
+# Your goal is to complete the assignments in such a way that you minimize the total lateness for the assignments. You need to return the minimum (Maximum lateness).
+#
+# Assume units for timetaken and deadline are days.
+#
+# For example:
+#
+# If assignments = [A, B, C, D]; timetaken=[1, 2, 3, 4] and deadline=[2,4,5,7].
+
