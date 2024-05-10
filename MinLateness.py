01f5d6bef5a02edaf9a205768482808ddb4d08b0
fantauzd <fantauzd@oregonstate.edu>
2024-05-09 22:29:24 -0400

    solved max lateness

--- MinLateness.py
+++ MinLateness.py
@@ -10,19 +10,30 @@
 #
 # If assignments = [A, B, C, D]; timetaken=[1, 2, 3, 4] and deadline=[2,4,5,7].
 
-def minLateness(Name, TimeTaken, Deadline):
+from operator import itemgetter
+
+def minLateness(assignments):
     """
     Solve the lateness problem and find the minimum acheivable late days for the given assignments.
-    :param Name: an array of assignments
-    :param TimeTaken: an array of the days needed to complete corresponding assignments
-    :param Deadline: an array of the deadline day for corresponding assignments
+    Assignment = [("A" , 2 , 2 ),( "B" , 3 , 4 ), ( "D" , 5 , 7 ), ( "C" , 1 , 6 )]
+    where the three values in each set are
+    :param Name:  assignment name
+    :param TimeTaken: the days needed to complete corresponding assignments
+    :param Deadline: the deadline day for corresponding assignments
     :return:
     """
     # sort all three arrays on deadline ASC
+    sorted_assignemnts = sorted([assignments], key = itemgetter(2))
 
     start_time = 0
     max_late = 0
     schedule = []
-    finish_time = 0
 
-    for i in range(len(Name)):
+    for assignment in assignments:
+        finish_time = start_time + assignment[1]
+        if finish_time > assignment[2]:
+            max_late = max(max_late, finish_time - assignment[2])
+        start_time = finish_time
+        schedule.append(assignment[0])
+
+    return max_late, schedule
